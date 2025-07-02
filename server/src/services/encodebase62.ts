18cebc4566169c8d212eec392e02fef09835c463
BiMalxMe <journeyxbimal@gmail.com>
2025-07-02 07:03:24 +0545

    deployfix003

--- server/src/services/encodebase62.ts
+++ server/src/services/encodebase62.ts
@@ -2,10 +2,10 @@ export function encodebase62(num : number){
     const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let result = "";
     
-    while(num > 0){
+    while(num > 0) {
         result = chars[num % 62] + result;
         num = Math.floor(num / 62)
     }
-    return result || "0"
+    return result || "0 "
 }
 
